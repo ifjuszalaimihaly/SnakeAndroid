b74fb050af690dc881f670365c5c6bdaa6ee0840
ifjuszalaimihaly@gmail.com <ifjuszalaimihaly@gmail.com>
2017-07-04 15:59:35 +0200

    gettop getleft

--- app/src/main/java/szalai/hu/snake/PlayActivity.java
+++ app/src/main/java/szalai/hu/snake/PlayActivity.java
@@ -1,6 +1,8 @@
 package szalai.hu.snake;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -9,7 +11,10 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class PlayActivity extends Activity implements SensorEventListener{
 
@@ -19,46 +24,54 @@ public class PlayActivity extends Activity implements SensorEventListener{
     private float x;
     private float y;
     private ImageView snakeBody;
+    RelativeLayout container;
     int width;
-    int heigt;
+    int height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        width = size.x;
-        heigt = size.y;
+        container = (RelativeLayout) findViewById(R.id.container);
+        snakeBody = (ImageView) findViewById(R.id.snakeimage);
+        /*ViewTreeObserver vto = container.getViewTreeObserver();
+        vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                container.getViewTreeObserver().removeOnPreDrawListener(this);
+                width = container.getWidth();
+                height = container.getHeight();
+                return true;
+            }
+        });*/
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
         sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
-        snakeBody = (ImageView) findViewById(R.id.snakeimage);
-
-
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        this.x = event.values[0];
-        this.y = event.values[1];
-        Log.i("info", x + " " + y);
-        if(x<0 && snakeBody.getX()<width-25){
-            snakeBody.setX(snakeBody.getX()+25);
+        this.x =  event.values[0];
+        this.y =  event.values[1];
+        //snakeBody.setLeft(0);
+        //snakeBody.setTop(0);
+        /*Log.i("info", snakeBody.getX() + " " + snakeBody.getY());*/
+
+
+        if(x<0){
+            snakeBody.setX(snakeBody.getLeft()+25);
             Log.i("info","right");
         }
-        if(x>0 && snakeBody.getX()>0){
-            snakeBody.setX(snakeBody.getX()-25);
+        if(x>0 && snakeBody.getLeft()>25){
+            snakeBody.setLeft(snakeBody.getLeft()-25);
             Log.i("info","left");
         }
-        if(y<0 && snakeBody.getY()<heigt-25){
-            snakeBody.setY(snakeBody.getY()+25);
+        if(y<0){
+            snakeBody.setTop(snakeBody.getTop()+25);
             Log.i("info","down");
         }
-        if(y>0 && snakeBody.getY()>0){
-            snakeBody.setY(snakeBody.getY()-25);
+        if(y>0 && snakeBody.getTop()>25){
+            snakeBody.setTop(snakeBody.getTop()-25);
             Log.i("info","up");
         }
     }
@@ -66,5 +79,34 @@ public class PlayActivity extends Activity implements SensorEventListener{
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private int getRelativeLeft(View myView) {
+        if (myView.getParent() == myView.getRootView())
+            return myView.getLeft();
+        else
+            return myView.getLeft() + getRelativeLeft((View) myView.getParent());
+    }
+
+    private int getRelativeTop(View myView) {
+        if (myView.getParent() == myView.getRootView())
+            return myView.getTop();
+        else
+            return myView.getTop() + getRelativeTop((View) myView.getParent());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this,sensor);
+    }
+
+    class MyGlobalListenerClass implements ViewTreeObserver.OnGlobalLayoutListener{
+        @Override
+        public void onGlobalLayout() {
+            View v = (View) findViewById(R.id.container);
+            width = v.getWidth();
+            height = v.getHeight();
+        }
     }
 }
