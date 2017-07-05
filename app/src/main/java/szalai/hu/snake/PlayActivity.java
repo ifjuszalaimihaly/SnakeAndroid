141c4b34d185f251f22b2a98ccf6887d52a3044b
ifjuszalaimihaly@gmail.com <ifjuszalaimihaly@gmail.com>
2017-07-05 12:26:59 +0200

    before create snake class

--- app/src/main/java/szalai/hu/snake/PlayActivity.java
+++ app/src/main/java/szalai/hu/snake/PlayActivity.java
@@ -1,24 +1,17 @@
 package szalai.hu.snake;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
-import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import szalai.hu.snake.entitiy.Enemy;
-
-import static szalai.hu.snake.R.drawable.enemy;
 
 public class PlayActivity extends Activity implements SensorEventListener {
 
@@ -35,13 +28,18 @@ public class PlayActivity extends Activity implements SensorEventListener {
     private boolean down;
     private boolean right;
     private boolean left;
+    private Enemy enemy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
         container = (RelativeLayout) findViewById(R.id.container);
-        snakeBody = (ImageView) findViewById(R.id.snakeimage);
+        snakeBody = new ImageView(this);
+        snakeBody.setX(0);
+        snakeBody.setY(0);
+        snakeBody.setImageResource(R.drawable.snakeimage);
+        container.addView(snakeBody);
         ViewTreeObserver vto = container.getViewTreeObserver();
         vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
@@ -55,12 +53,10 @@ public class PlayActivity extends Activity implements SensorEventListener {
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        ImageView enemyView = new ImageView(this);
-        enemyView.setImageResource(enemy);
-        Enemy enemy = new Enemy(39, 65);
-        enemyView.setX(250);
-        enemyView.setY(250);
-        container.addView(enemyView);
+        enemy = new Enemy(this, 2, 2);
+        container.addView(enemy.getImageView());
+
+
     }
 
     @Override
@@ -76,22 +72,30 @@ public class PlayActivity extends Activity implements SensorEventListener {
     }
 
     private void stearing() {
-        Log.i("info", snakeBody.getX() + " " + snakeBody.getY());
-        if (x < 2 && snakeBody.getX() < width - 75) {
+        Log.i("info", "snake " + snakeBody.getX() + " " + snakeBody.getY());
+        Log.i("info", "enemy " + enemy.getImageView().getX() + " " + enemy.getImageView().getY());
+        Log.i("info", "width " + width + " height " + height);
+        if (x < 1 && snakeBody.getX() < width - 75) {
             snakeBody.setX(snakeBody.getX() + 25);
-            Log.i("info", "right");
+            //Log.i("info", "right");
         }
-        if (x > -2 && snakeBody.getX() > 25) {
+        if (x > -1 && snakeBody.getX() > 0) {
             snakeBody.setX(snakeBody.getX() - 25);
-            Log.i("info", "left");
+            //Log.i("info", "left");
         }
-        if (y > 2 && snakeBody.getY() < height - 75) {
+        if (y > 1 && snakeBody.getY() < height - 75) {
             snakeBody.setY(snakeBody.getY() + 25);
-            Log.i("info", "down");
+            //Log.i("info", "down");
         }
-        if (y < -2 && snakeBody.getY() > 25) {
+        if (y < -1 && snakeBody.getY() > 0) {
             snakeBody.setY(snakeBody.getY() - 25);
-            Log.i("info", "up");
+            //Log.i("info", "up");
+        }
+
+        if (enemy.getImageView().getX() == snakeBody.getX() && enemy.getImageView().getY() == snakeBody.getY()) {
+            container.removeView(enemy.getImageView());
+            enemy = new Enemy(this, 2, 2);
+            container.addView(enemy.getImageView());
         }
     }
 
--- app/src/main/java/szalai/hu/snake/entitiy/Enemy.java
+++ app/src/main/java/szalai/hu/snake/entitiy/Enemy.java
@@ -1,6 +1,12 @@
 package szalai.hu.snake.entitiy;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.ImageView;
+
 import java.util.Random;
+
+import szalai.hu.snake.R;
 
 /**
  * Created by Mihaly Szalai on 2017. 07. 04..
@@ -13,28 +19,31 @@ public class Enemy {
     private int xlenght;
     private int ylenght;
     private Random rnd;
+    private ImageView imageView;
 
-    public Enemy(int xlenght, int ylenght){
+    public Enemy(Context context, int xlenght, int ylenght) {
         this.xlenght = xlenght;
         this.ylenght = ylenght;
         enemyXpos = new int[xlenght];
         enemyYpos = new int[ylenght];
         rnd = new Random();
         for(int i=0; i<xlenght; i++){
-            enemyXpos[i] = i*25;
+            enemyXpos[i] = i * 50;
         }
         for(int i=0; i<ylenght; i++){
-            enemyYpos[i] = i*25;
+            enemyYpos[i] = i * 50;
         }
+        for (int i = 0; i < xlenght; i++) {
+            Log.i("info", "enemy x " + enemyYpos[i]);
+        }
+        Log.i("info", xlenght + " " + ylenght);
+        imageView = new ImageView(context);
+        imageView.setImageResource(R.drawable.enemy);
+        imageView.setX(enemyXpos[rnd.nextInt(xlenght)]);
+        imageView.setY(enemyYpos[rnd.nextInt(ylenght)]);
     }
 
-    public int getXPos(){
-        return enemyXpos[rnd.nextInt(xlenght)];
+    public ImageView getImageView() {
+        return imageView;
     }
-
-    public int getYPos(){
-        return enemyYpos[rnd.nextInt(ylenght)];
-    }
-
-
 }
